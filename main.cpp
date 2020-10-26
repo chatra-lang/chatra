c890d9feb1420017655217f6ef27c9c7e0c6fff8
Satoshi Hosokawa <chatra.hosokawa@gmail.com>
2020-10-26 22:29:19 +0900

    Fix an issue that the frontend misunderstands some statement as non-block statement (fixes a message)

--- main.cpp
+++ main.cpp
@@ -1335,7 +1335,7 @@ static void processDebuggerCommand(const std::string& input) {
 		if (t0Length != 0) {
 			auto firstToken = input.substr(0, t0Length);
 			if (firstToken == "catch" || firstToken == "finally") {
-				dError("%s block on top-level was ignored since it has no effect in interactive mode.");
+				dError("warning: %s block on top-level was ignored since it has no effect in interactive mode.");
 				continue;
 			}
 		}
